d8b72d6d9e8db5ee1e2afcc3ae7bfb503d14199a
sun532818microbit <114642649+sun532818microbit@users.noreply.github.com>
2023-02-15 14:55:55 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+grove.onGesture(GroveGesture.Down, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+grove.onGesture(GroveGesture.Up, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
